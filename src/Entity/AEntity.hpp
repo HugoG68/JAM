a025db0d7a7402aae81a6a43ea71eb555fb5a457
Evan CANDAN <114905170+evancandan@users.noreply.github.com>
2024-05-18 02:19:57 +0200

    feat: 🎸 Fuel Bar for Player

--- src/Entity/AEntity.hpp
+++ src/Entity/AEntity.hpp
@@ -20,25 +20,25 @@ private:
 public:
     AEntity(std::tuple<double, double> pos, double speed) :_pos(pos), _speed(speed) {};
     ~AEntity() = default;
-    void go_up(void) {
+    virtual void go_up(void) {
         double pos_y = std::get<1>(_pos);
         double new_pos_y = pos_y - _speed; 
         if (new_pos_y >= 0.0)
             std::get<1>(_pos) = new_pos_y;
     }
-    void go_left(void){
+    virtual void go_left(void){
         double pos_x = std::get<0>(_pos);
         double new_pos_x = pos_x - _speed; 
         if (new_pos_x >= 0.0)
             std::get<0>(_pos) = new_pos_x;
     }
-    void go_right(void){
+    virtual void go_right(void){
         double pos_x = std::get<0>(_pos);
         double new_pos_x = pos_x + _speed; 
         if (new_pos_x <= 1.0)
             std::get<0>(_pos) = new_pos_x;
     }
-    void go_down(void){
+    virtual void go_down(void){
         double pos_y = std::get<1>(_pos);
         double new_pos_y = pos_y + _speed; 
         if (new_pos_y <= 1.0)
